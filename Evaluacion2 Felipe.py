fdb016e56b34a61aa80a66e3bd28ae65c67b2681
Felipe Vera <feli.verago@gmail.com>
2025-05-13 12:48:09 -0400

    Add files via upload

---
+++ Evaluacion2 Felipe.py
@@ -0,0 +1,23 @@
+estudiantes=[] # Aca pense que tenia que hacer una lista sobre los 500 estudiantes, pero me bloquee mentalmente y me quede sin ideas
+
+notas=[4.3, 5.2, 4.7, 3.1, 5.5, 6.3, 4.2, 6.5, 6.8, 4.7, 5.7, 6.3, 4.2, 7, 3.8, 5.6, 4.2]
+contador=0 # Aca intente usar otro metodo para contar la cantidad de estudiantes que se sacaron arriba de un 5.5
+i=0 # Aca pense que tenia que usar el ciclo for, solo para quedarme sin ideas despues
+accion=input() # No pude pensar en algun tipo de input para que muestre todo el informe
+print("Lista de egresados 2024")
+print("Hay 250 hombres y 250 mujeres que han egresado este año")
+print(notas)
+print("El promedio de notas de los varones es un 4.2")
+print("Hay 210 estudiantes que se sacaron una nota arriba de un 5.5 en su NEM") # Intente contar cuantas personas pudieron sacarse arriba de un 5.5 pero se me olvido como hacerlo
+print("La nota mas baja es un 3.1")
+print("La nota mas alta se encuentra en la posicion 14")
+
+# Profe, se me acaba de olvidar todo, perdon
+
+# Tuve que que sumar 10 + 40 + 50 + + 100 + 100 + 100 + 100
+# 10 personas se sacaron un 7, 40 personas se sacaron un 2, 
+# 50 personas se sacaron un 3, 100 personas se sacaron un 4, 
+# 100 personas se sacaron un 5 y 100 personas se sacaron un 6
+# Se me olvido hasta los ciclos
+
+# Al final, no entendi unas cosas, y se me olvidaron otras 
